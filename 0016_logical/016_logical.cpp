b884b7561169a4f2c9da0d2021b4116e7572ab45
ljy0711 <wjddus5696@naver.com>
2024-09-04 14:49:42 +0900

    Add files via upload
    2주차 수업 내용

---
+++ 0016_logical/016_logical.cpp
@@ -0,0 +1,15 @@
+﻿// 0016_logical.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 3;
+
+    bool p = a >= 1 && a <= 10;
+    bool q = a == 3|| a == 7;
+    bool r = !q;
+
+    printf("%d %d %d\n", p, q, r);
+}
