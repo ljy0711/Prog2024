8c7ae02b55e7cddb07721f905f6624ee851731d9
ljy0711 <wjddus5696@naver.com>
2024-10-22 14:47:55 +0900

    Add files via upload

---
+++ 045_2DArray/045_2DArray.cpp
@@ -0,0 +1,27 @@
+﻿// 045_2DArray.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	int a[3][4] = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
+
+	printf("%d\n", &a[0]);
+	printf("%d\n", a);
+	printf("%d\n", &a[1]);
+	printf("%d\n", a+1);
+	printf("%d\n", &a[0]+1);
+
+
+	int (*p)[4] = &a[0]; //정수4개짜리의 포인터(배열을 갖음)
+
+	for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < 4; i++) {
+			printf("%4d", (*(p + j))[i]);
+		}
+		printf("\n");
+	}
+		
+}
+
