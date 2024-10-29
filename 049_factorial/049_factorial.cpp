39a3fc253594be6c0c82c762767f97e1ad39ad4a
ljy0711 <wjddus5696@naver.com>
2024-10-29 14:48:21 +0900

    Add files via upload

---
+++ 049_factorial/049_factorial.cpp
@@ -0,0 +1,44 @@
+﻿// 049_factorial.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+/*숫자 n을 입력받아서 n!을 계산하시오
+(1) 반복문으로
+(2) 재귀함수를 사용해서 */
+
+#include <stdio.h>
+//재귀함수 (2)
+int factorial(int n); //함수의 원형(프로토타입)
+
+int main()
+{
+	int n;
+	printf("n 입력 : ");
+	scanf_s("%d", &n);
+	//반복문
+	long long a = 1;
+	for (int i = 1; i <= n; i++) {
+		a = 1;
+		for (int j = 1; j <= i; j++)
+			a *= j;
+		printf("%d! = %lld\n", i, a);
+	}
+	//1부터 100까지 반복문 합
+	int sum = 0;
+	for (int i = 1; i <= 100; i++) {
+		sum += i;
+	}
+	printf("1부터 100까지의 합 : %d\n",sum);
+
+	//1부터 n까지 반복문 곱
+	int p = 1;
+	for (int i = 1; i <= n; i++)
+		p *= i;
+	printf("1부터 n까지의 곱 : %d\n",p);
+
+	// 재귀함수 (1)
+	printf("%d\n", factorial(n));
+
+} // 재귀함수 (3)
+int factorial(int n) {
+	if (n == 1) // 재귀함수는 끝나는 조건이 꼭 있어야함
+		return 1;
+	return factorial(n - 1) * n; //팩토리얼 
+}
---
+++ 050_printArray/050_printArray.cpp
@@ -0,0 +1,48 @@
+﻿// 050_printArray.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <stdio.h>
+void printArr(int a[], int n)
+//그냥 출력하는 거기 때문에 void 사용
+//앞에는 배열 뒤는 숫자 
+{
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+}
+
+/* 
+void printArr(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+} 
+*/
+
+void changeArr(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+		a[i] *= a[i];
+}
+
+//2차원 배열을 매개변수로 전달받는 방법
+void printArr2(int a[][4], int n) //[4]뒤에 거는 꼭 쓰기
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 4; j++)
+			printf("%4d", a[i][j]);
+		printf("\n");
+	}	
+}
+
+int main()
+{
+	int a[] = { 1,2,3,4,5 };
+	//2차원 배열 함수
+	int b[3][4] = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
+
+	printArr2(b, 3); //2차원 배열
+	printArr(a, 5);
+	changeArr(a, 5);
+}
+
